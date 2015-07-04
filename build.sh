a39adf6a96a81944ab8ef037f6ee0d5c8ef1072e
Patrick Steiner <patrick@steiner-buchholz.de>
2015-07-04 23:27:47 +0200

    corrected dashboard

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 echo "-Building deloyables"
 
-export BUILD_LOGFILE="./build.log"
+export BUILD_LOGFILE="../build.log"
 
 echo "-- client"
 pushd . > /dev/null
@@ -13,37 +13,37 @@ popd > /dev/null
 echo "-- iot_controller_receiver"
 pushd . > /dev/null
 cd iot_controller_receiver
-mvn clean install > $BUILD_LOGFILE 
+mvn clean install >> $BUILD_LOGFILE 
 popd > /dev/null
 
 echo "-- iot_controller_router"
 pushd . > /dev/null
 cd iot_controller_router
-mvn clean install >  $BUILD_LOGFILE
+mvn clean install >>  $BUILD_LOGFILE
 popd > /dev/null
 
 echo "-- iot_controller_temperature"
 pushd . > /dev/null
 cd iot_controller_temperature
-mvn clean install >  $BUILD_LOGFILE
+mvn clean install >>  $BUILD_LOGFILE
 popd > /dev/null
 
 echo "-- iot_controller_cep"
 pushd . > /dev/null
 cd iot_controller_cep
-mvn clean install >  $BUILD_LOGFILE
+mvn clean install >>  $BUILD_LOGFILE
 popd > /dev/null
 
 echo "-- iot_datacenter_receiver"
 pushd . > /dev/null
 cd iot_datacenter_receiver
-mvn clean install >  $BUILD_LOGFILE
+mvn clean install >>  $BUILD_LOGFILE
 popd > /dev/null
 
 echo "-- iot_datacenter_monitor"
 pushd . > /dev/null
 cd iot_datacenter_monitor
-mvn clean install >  $BUILD_LOGFILE
+mvn clean install >>  $BUILD_LOGFILE
 popd > /dev/null
 
 echo "-Building all images for Fuse IoT Demo"
