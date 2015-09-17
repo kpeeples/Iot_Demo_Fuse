c8909a0f613ba17a98a42b1a903bee6007b3a36c
Patrick Steiner <patrick@steiner-buchholz.de>
2015-09-17 17:52:52 +0200

    OSE work

--- run.sh
+++ run.sh
@@ -7,6 +7,8 @@ export count="50"
 export waitTime="1"
 export messageFormat="XML"
 export receiverURL="iot-demo.cloudapps.example.com"
+export defaultPort=":30061"
+
 
 echo "Starting the producer to send messages "
-java -DreceiverURL=$receiverURL -DmessageFormat=$messageFormat -DdeviceType=$deviceType -DdeviceID=$deviceID -DinitialValue=$initialValue -Dcount=$count -DwaitTime=$waitTime -jar producer/target/iot_producer-jar-with-dependencies.jar
+java -DreceiverURL=$receiverURL -DdefaultPort=$defaultPort -DmessageFormat=$messageFormat -DdeviceType=$deviceType -DdeviceID=$deviceID -DinitialValue=$initialValue -Dcount=$count -DwaitTime=$waitTime -jar producer/target/iot_producer-jar-with-dependencies.jar
