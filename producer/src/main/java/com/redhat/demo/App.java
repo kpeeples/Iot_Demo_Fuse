79e92af6c96817246fc7436fc33032458f3821eb
Patrick Steiner <patrick@steiner-buchholz.de>
2015-09-17 18:22:14 +0200

    Updated for OSEV3

--- producer/src/main/java/com/redhat/demo/App.java
+++ producer/src/main/java/com/redhat/demo/App.java
@@ -11,9 +11,9 @@ import com.redhat.demo.iot_controller_data.DummyDataGenerator;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
-	
+
 	// Default Values for message producer
     private static final String	DEFAULT_DEVICETYPE   = "1";
     private static final String DEFAULT_DEVICEID     = "1";
@@ -23,17 +23,18 @@ public class App
     private static final String DEFAULT_WAIT		 = "1";
     private static final String DEFAULT_FORMAT		 = "XML";
     private static final String DEFAULT_RECEIVER	= "localhost";
+    private static final String DEFAULT_PORT		= "61616";
 
-	 
+
     public static void main( String[] args ) throws Exception
     {
-    	
+
     	DummyDataGenerator dummy = null;
-        
-   	
+
+
     	// Create data to send
         dummy = new DummyDataGenerator();
-        
+
         int devType 	 = Integer.parseInt( System.getProperty("deviceType", DEFAULT_DEVICETYPE));
         int devID		 = Integer.parseInt( System.getProperty("deviceID", DEFAULT_DEVICEID));
         int initialValue = Integer.parseInt(System.getProperty("initialValue", DEFAULT_INITIALVALUE));
@@ -41,20 +42,21 @@ public class App
         int waitTime = Integer.parseInt(System.getProperty("waitTime", DEFAULT_WAIT));
         String messageFormat = System.getProperty("messageFormat",DEFAULT_FORMAT);
         String unit			 = System.getProperty("payloadUnit", DEFAULT_UNIT);
-        String brokerURL = "tcp://" + System.getProperty("receiverURL",DEFAULT_RECEIVER) + ":61616" ;
-        
-        dummy.createInitialDataSet(devType, devID, initialValue, unit); 
+        String port			 = System.getProperty("defaultPort", DEFAULT_PORT);
+        String brokerURL = "tcp://" + System.getProperty("receiverURL",DEFAULT_RECEIVER) + port ;
+
+        dummy.createInitialDataSet(devType, devID, initialValue, unit);
 
         // setup the connection to ActiveMQ
-    	ConnectionFactory factory = new ActiveMQConnectionFactory("admin", "admin", brokerURL);
-        
+    		ConnectionFactory factory = new ActiveMQConnectionFactory("admin", "admin", brokerURL);
+
         Producer producer = new Producer(factory, "message.receive" );
-        
+
         System.out.println(dummy.getDataSetCSV());
-        
+
         int counter = 0;
         while ( counter < count ) {
-			
+
         	if ( messageFormat.equals("XML")) {
         		System.out.println("Sending as XML");
         		producer.run( dummy.getDataSetXML() );
@@ -62,15 +64,15 @@ public class App
         		System.out.println("Sending as CSV");
         		producer.run( dummy.getDataSetCSV() );
         	}
-        		
-        	
+
+
 			dummy.updateDataSet();
-			
+
 			counter++;
-			
+
 			Thread.sleep ( waitTime * 1000 );
         }
-		    
+
         producer.close();
     }
 }
